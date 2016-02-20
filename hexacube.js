8dfa0f58fe59c25b86f1e6cdd5051bef16d23a57
jazzcript <artur.alves@gmail.com>
2016-02-20 22:44:16 +0000

    And so it begins, click for light cubes

--- hexacube.js
+++ hexacube.js
@@ -1,31 +1,34 @@
-// inject html into page
-
 jazz.loadStyle("hexacube.css");
 
 var cols = 16, rows = 16;
 
 $.get("hexacube.html", function (cube) {
-    var body = $("body"), page = $('<div class="col" />'), skull = 10;
+    var body = $("body"), col = $('<div class="col" />'), skull = 10;
     
     cube = $(cube);
+    
+    body.on("click", "div", function(){
+        var box = $(this).parents(".box-wrapper");
+        console.log(box);
+        box.addClass("active");
+        return false;
+    });
 
     console.log("cube", cube);
 
-    while(skull--) page.append(cube.clone());
+    while(skull--) col.append(cube.clone(true));
 
-    //body.append(page);
-    
     var marginCounter = 0.9;
     var marginIncrement = 0.9;
     var newCol;
     
     while (cols--) {
         marginCounter += marginIncrement;
-        newCol = page.clone().css({margin:"1.6em " + marginCounter + "em"});
+        newCol = col.clone().css({margin:"1.6em " + marginCounter + "em"});
         body.append(newCol);
         
         marginCounter += marginIncrement;
-        newCol = page.clone().css({margin:"0 " + marginCounter + "em"});
+        newCol = col.clone().css({margin:"0 " + marginCounter + "em"});
         body.append(newCol);
     }
 });
